c856a6b04025884a39b6bce3e08ef9e186afe9e4
camilo Navas <navasochoa@hotmail.com>
2023-11-30 19:38:26 -0500

    inicio nodejs

---
+++ 00_HolaMundo/app.js
@@ -0,0 +1,15 @@
+let nombre = 'Camilo'
+
+console.log(`Hola mundo, mi nombre es ${nombre}, este es mi primer mensaje desde Node.js`)
+
+let edad = 39
+
+if(edad >= 18) console.log('Eres mayor de edad')
+else console.log('Eres mayor de edad', edad)
+
+
+function suma(a, b) {
+    return a + b
+}
+
+console.log(suma(19,20))
