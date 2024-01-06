6dc223bc93fe3dbfd3de1d06043f92783f778d64
camilo Navas <navasochoa@hotmail.com>
2024-01-06 16:52:58 -0500

    inicio toDoApp y nodemon

---
+++ 05_proyecto/ToDoCLI/app.js
@@ -0,0 +1,20 @@
+import { createInterface } from "readline"
+import chalk from "chalk"
+
+const tasks = []
+
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+
+function displayMenu() {
+    console.log(chalk.yellow.bold("↨↨To Do App↨↨"))
+    console.log("1. Agregar tarea")
+    console.log("2. Listar tareas")
+    console.log("3. Completar tarea")
+    console.log("4. Salir")
+}
+
+displayMenu()
