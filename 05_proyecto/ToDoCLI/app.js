ae7ca802e4ef3eb022b45af84ef82a4cdaa2d378
camilo Navas <navasochoa@hotmail.com>
2024-01-08 18:40:35 -0500

    comunicacion con la CLI

--- 05_proyecto/ToDoCLI/app.js
+++ 05_proyecto/ToDoCLI/app.js
@@ -11,10 +11,39 @@ const rl = createInterface({
 
 function displayMenu() {
     console.log(chalk.yellow.bold("↨↨To Do App↨↨"))
+    console.log(chalk.blueBright("Menu de Opciones"))
     console.log("1. Agregar tarea")
     console.log("2. Listar tareas")
     console.log("3. Completar tarea")
     console.log("4. Salir")
 }
 
+function chooseOption() {
+    rl.question("Elige una opción, digita el número de tu opción:", (choice) => {
+        switch (choice) {
+            case "1": 
+                console.log("Crear Tarea")
+                break;
+            case "2": 
+                console.log("Listar Tareas")
+                break;
+            case "3": 
+                console.log("Completando Tarea")
+                console.log("Tarea Completada")
+                break;
+            case "4": 
+                console.log(chalk.yellow("Adiós"))
+                rl.close()
+                break;
+            default:
+                console.log(chalk.red("Opción Inválida, intenta nuevamente, \n"))
+                displayMenu()
+                chooseOption()
+                break;
+        }
+    }    
+    )}
+    
+
 displayMenu()
+chooseOption()
