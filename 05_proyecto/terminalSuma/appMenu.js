a9bc9de7611a4d5380d44c671a73e49dbfe17ff7
camilo Navas <navasochoa@hotmail.com>
2024-01-24 18:01:21 -0500

    resta en terminal

--- 05_proyecto/terminalSuma/appMenu.js
+++ 05_proyecto/terminalSuma/appMenu.js
@@ -6,7 +6,7 @@ const rl = createInterface({
   output: process.stdout,
 });
 
-const numbers = [];
+let numbers = [];
 
 function displayMenu() {
   console.log(
@@ -29,18 +29,46 @@ function displayMenu() {
 //   });
 // }
 
+function añadirNum() {
+    rl.question(chalk.red("Digita nuevo número: "), (num) => {
+        numbers.push(num)
+        let cont = 0;
+        for (let i = 0; i < numbers.length; i++) {
+          cont = cont + parseInt(numbers[i]);
+        }
+        console.log(chalk.green(`La suma da ${cont}\n`));
+        volverOAñadir();
+        optionVolver();
+    })
+}
+
+function volverOAñadir() {
+  console.log(chalk.black("¿Qué deseas hacer?"));
+  console.log(chalk.black("1. Añadir otro número"));
+  console.log(chalk.black("2. Volver al menú principal"));
+}
+
 function sumaDeNumeros() {
-    let cont = 0
-    for (let i = 0; i < numbers.length; i++) {
-        cont = cont + parseInt(numbers[i])
-    }
-    console.log(chalk.green(`La suma da ${cont}`))
+  let cont = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    cont = cont + parseInt(numbers[i]);
+  }
+  console.log(chalk.green(`La suma da ${cont}\n`));
+  volverOAñadir();
+  optionVolver();
 }
 
 function nuevaSuma() {
-    rl.question(chalk.red("Digita el segundo número: "), (num2) => {
-        numbers.push(num2);
-        sumaDeNumeros();
+  rl.question(chalk.red("Digita el segundo número: "), (num2) => {
+    numbers.push(num2);
+    sumaDeNumeros();
+  });
+}
+
+function nuevaResta(num) {
+    rl.question(chalk.red("Digita el número para restar: "), (num2) => {
+        let resta = parseInt(num) - parseInt(num2)
+        console.log(chalk.green(`La resta de ${num} - ${num2} es ${resta}`))
     })
 }
 
@@ -52,6 +80,33 @@ function suma() {
   });
 }
 
+function resta() {
+    rl.question(chalk.red("Digita el primer número: "), (num) => {
+        console.log(`Introduciste ${num}`);
+        nuevaResta(num);
+    })
+}
+
+function optionVolver() {
+  rl.question(chalk.red("Respuesta: "), (choice) => {
+    switch (choice) {
+      case "1":
+        añadirNum();
+        break;
+      case "2":
+        displayMenu();
+        chooseOption();
+        numbers = []
+        break;
+      default:
+        displayMenu();
+        chooseOption();
+        numbers = []
+        break;
+    }
+  });
+}
+
 function chooseOption() {
   rl.question(chalk.red("Respuesta: "), (choice) => {
     switch (choice) {
@@ -59,6 +114,7 @@ function chooseOption() {
         suma();
         break;
       case "2":
+        resta();
         break;
       case "3":
         break;
