d5d7c11a899b5b0c941189e2c4eae3a35117ccc6
camilo Navas <navasochoa@hotmail.com>
2024-01-26 18:48:09 -0500

    express server

--- principiosHttpServidor/index.js
+++ principiosHttpServidor/index.js
@@ -1,14 +1,58 @@
 console.clear()
 import { createServer } from "http"
+import express from 'express'
 
-const httpServer = createServer((req, res) => {
-    //Nos falta el verbo/método para indicar que quiere hacer el cliente
+const PORT = 3000
+const expressApp = express()
 
-    console.log(req.method);
-    //Nos falta el path/ruta para identificar el recurso
-    console.log(req.url);
-    // console.log("PETICIÓN RECIBIDA");
-    res.end("RECIBIDO ;)") 
+// expressApp.get('/mi-cuenta', (req, res) => {
+//     res.send('Tu cuenta personal')
+// })
+
+expressApp.use(express.json())
+expressApp.use(express.text())
+
+expressApp.post("/cuenta", (req, res) => {
+    console.log(req.query)
+    res.send()
 })
 
-httpServer.listen(3000) 
+expressApp.put('/cuenta/:idcuenta', (req, res) => {
+    // console.log(req.params.idcuenta);
+    // console.log(req.params)
+    // console.log(req.headers)
+    // console.log(req.get("noexiste"));
+    // res.send('Tu cuenta personal')
+    // res.status(401).send({
+    //     errorMessage: "No autorizado",
+    // })
+    console.log(req.body)
+    res.send()
+})
+
+expressApp.listen(PORT, () => console.log(`Servidor levantado en el puerto ${PORT}`))
+
+// const httpServer = createServer((req, res) => {
+//     //Nos falta el verbo/método para indicar que quiere hacer el cliente
+
+//     console.log(req.method);
+//     //Nos falta el path/ruta para identificar el recurso
+//     console.log(req.url);
+//     //Nos faltan las cabeceras
+//     console.log(req.headers)
+//     //nos falta el body/payload
+//     let data = ''
+//     let chunkIndex = 0
+//     req.on('data', (chunk) => {
+//         data += chunk
+//         chunkIndex++
+//         console.log(chunkIndex);
+//     })
+//     req.on('end', () => {
+//         console.log(data)
+//     })
+//     // console.log("PETICIÓN RECIBIDA");
+//     res.end("RECIBIDO ;)") 
+// })
+
+// httpServer.listen(3000)
