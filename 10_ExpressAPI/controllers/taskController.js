5230ec31f3e2d7372c2073591115064e1e95b60c
camilo Navas <navasochoa@hotmail.com>
2024-02-17 09:22:19 -0500

    10_ExpressAPI

---
+++ 10_ExpressAPI/controllers/taskController.js
@@ -0,0 +1,86 @@
+let tasks = [
+  { id: 1, title: "Tarea 1", completed: false },
+  { id: 2, title: "Tarea 2", completed: true },
+];
+
+const getAllTasks = (req, res) => {
+  res.json({ tasks });
+};
+
+const addTask = (req, res) => {
+  //console.log(req.body);
+  let { title } = req.body;
+  let id = tasks.length + 1;
+  tasks.push({ id, title, completed: false });
+  res.json({ err: false, message: "Tarea Agregada" });
+};
+
+const editTask = (req, res) => {
+  let id = parseInt(req.params.id);
+  let taskIndex = tasks.findIndex((task) => task.id === id);
+  //console.log(taskIndex);
+
+  if (taskIndex === -1) {
+    res.status(404).json({ err: true, message: "Tarea no encontrada" });
+  } else {
+    tasks[taskIndex].title = req.body.title;
+    res.json({ task: tasks[taskIndex] });
+  }
+};
+
+const getTask = (req, res) => {
+  let id = parseInt(req.params.id);
+  let taskIndex = tasks.findIndex((task) => task.id === id);
+  //console.log(taskIndex);
+
+  if (taskIndex === -1) {
+    res.status(404).json({ err: true, message: "Tarea no encontrada" });
+  } else {
+    res.json({ task: tasks[taskIndex] });
+  }
+};
+
+const completeTask = (req, res) => {
+  let id = parseInt(req.params.id);
+  let task = tasks.find((task) => task.id === id);
+
+  if (task) {
+    task.completed = true;
+    res.json({ err: false, message: "Tarea completada" });
+  } else {
+    res.status(404).json({ err: true, message: "Tarea no encontrada" });
+  }
+};
+
+const uncompleteTask = (req, res) => {
+  let id = parseInt(req.params.id);
+  let task = tasks.find((task) => task.id === id);
+
+  if (task) {
+    task.completed = false;
+    res.json({ err: false, message: "Tarea no completada" });
+  } else {
+    res.status(404).json({ err: true, message: "Tarea no encontrada" });
+  }
+};
+
+const deleteTask = (req, res) => {
+  let id = parseInt(req.params.id);
+  let taskIndex = tasks.findIndex((task) => task.id === id);
+  if (taskIndex === -1) {
+    res.status(404).json({ err: true, message: "Tarea no encontrada" });
+  } else {
+    tasks.splice(taskIndex, 1);
+    res.json({ err: false, message: "Tarea eliminada" });
+  }
+};
+
+export default {
+  getAllTasks,
+  addTask,
+  editTask,
+  getTask,
+  completeTask,
+  uncompleteTask,
+  deleteTask,
+};
